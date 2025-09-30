e9ced394135925380e8055bb4c55783b4af16699
jeocamrank <104308268+jeocamrank@users.noreply.github.com>
2025-09-30 13:26:25 +0700

    finish setup frontend

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,24 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-type test = string;
+const isDev = !app.isPackaged; // true khi chạy dev, false khi build
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (isDev) {
+    // 🔥 Dev mode → load Vite server (Clerk sẽ nhận http://localhost:3000)
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.webContents.openDevTools();
+  } else {
+    // 🚀 Production → load file build React
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  }
 });
---
+++ src/firebase/fire.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5Br8eZkk6U_Kwkh2V17bPT_UmlHpWeBg",
+  authDomain: "music-app-53622.firebaseapp.com",
+  projectId: "music-app-53622",
+  storageBucket: "music-app-53622.firebasestorage.app",
+  messagingSenderId: "498680002889",
+  appId: "1:498680002889:web:a7932bb47aefebb1644298",
+  measurementId: "G-MD209YMWK3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
